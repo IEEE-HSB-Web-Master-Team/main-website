3637ae9a21a62fe65aea82339bf9de167da36e13
Youssef Shafek <youssefmsa616@gmail.com>
2024-10-17 11:46:45 +0300

    add fonts and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Ubuntu Fonts
+        'ubuntu': ['"Ubuntu"', 'sans-serif'],
+        'ubuntu-bold': ['"Ubuntu Bold"', 'sans-serif'],
+        'ubuntu-bold-italic': ['"Ubuntu Bold Italic"', 'sans-serif'],
+        'ubuntu-light': ['"Ubuntu Light"', 'sans-serif'],
+        'ubuntu-light-italic': ['"Ubuntu Light Italic"', 'sans-serif'],
+        'ubuntu-medium': ['"Ubuntu Medium"', 'sans-serif'],
+        'ubuntu-medium-italic': ['"Ubuntu Medium Italic"', 'sans-serif'],
+        'ubuntu-regular': ['"Ubuntu Regular"', 'sans-serif'],
+
+        // Helvetica Fonts
+        'helvetica': ['"Helvetica"', 'sans-serif'],
+        'helvetica-bold': ['"Helvetica Bold"', 'sans-serif'],
+        'helvetica-bold-oblique': ['"Helvetica Bold Oblique"', 'sans-serif'],
+        'helvetica-compressed': ['"Helvetica Compressed"', 'sans-serif'],
+        'helvetica-light': ['"Helvetica Light"', 'sans-serif'],
+        'helvetica-oblique': ['"Helvetica Oblique"', 'sans-serif'],
+        'helvetica-rounded-bold': ['"Helvetica Rounded Bold"', 'sans-serif'],
+
+        // Uni Sans Fonts
+        'uni-sans-heavy': ['"Uni Sans Heavy"', 'sans-serif'],
+        'uni-sans-heavy-italic': ['"Uni Sans Heavy Italic"', 'sans-serif'],
+        'uni-sans-thin': ['"Uni Sans Thin"', 'sans-serif'],
+        'uni-sans-thin-italic': ['"Uni Sans Thin Italic"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
