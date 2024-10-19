1954bc802a9b2c65d24e267fefb8590b2853152f
cheetah-10 <mennashehata2005@gmail.com>
2024-10-19 18:54:06 +0300

    chairpersons slider and our chapters components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { useEffect } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css'
-import WOW from 'wow.js';
-import 'animate.css';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import About from './components/About/About';
+import Home from './components/Home/Home';
 
 let routers = createBrowserRouter([
   {
     path: '' , element:<Layout/> , children:[
-      {index:true, element:<About/>},
-      {path:'about' , element:<About/>}
+      {index:true, element:<Home/>},
+      {path:'about' , element:<About/>},
+      {path:'home' , element:<Home/>}
     ]
   }
 ])
 
 function App() {
 
-  useEffect(() => {
-    const wow = new WOW({
-      boxClass: 'wow',
-      animateClass: 'animate__animated',
-      offset: 100,
-      mobile: true,
-      live: true
-    });
-    wow.init();
-  }, []);
+  
 
 
   return (
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import ChairPersonsSlider from "../ChairPersonsSlider/ChairPersonsSlider";
+import OurChapters from "../OurChapters/OurChapters";
+
+
+function Home() {
+    return ( <>
+    
+    <ChairPersonsSlider/>
+    <OurChapters/>
+
+    </> );
+}
+
+export default Home;
